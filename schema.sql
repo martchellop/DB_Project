54ca63ea752a2998d989f155c975c21881c78c88
Bruno Gomes Coelho <bruno.gomes.coelho@usp.br>
2018-05-28 13:37:03 -0300

    Add not null for primary keys
    For increase readability for whoever populates the table

--- schema.sql
+++ schema.sql
@@ -1,5 +1,5 @@
 CREATE TABLE organizador (
-	CPF char(11),
+	CPF char(11) NOT NULL,
 	nome varchar(50) NOT NULL,
 	email varchar(50),
 	telefone varchar(15),
@@ -7,15 +7,15 @@ CREATE TABLE organizador (
 );
 
 CREATE TABLE festa_tipo (
-	data timestamp,
-	organizador char(11),
+	data timestamp NOT NULL,
+	organizador char(11) NOT NULL,
 	tipo varchar(21) NOT NULL,
 	CONSTRAINT pk_festa_tipo PRIMARY KEY (data, organizador)
 );
 
 CREATE TABLE universitaria (
-	data timestamp,
-	organizador char(11),
+	data timestamp NOT NULL,
+	organizador char(11) NOT NULL,
 	preco smallint NOT NULL,
 	CONSTRAINT pk_universitaria PRIMARY KEY (data, organizador),
 	CONSTRAINT fk_universitaria_org FOREIGN KEY (organizador)
@@ -25,9 +25,9 @@ CREATE TABLE universitaria (
 );
 
 CREATE TABLE bilhete (
-	data timestamp,
-	organizador char(11),
-	id serial,
+	data timestamp NOT NULL,
+	organizador char(11) NOT NULL,
+	id serial NOT NULL,
 	preco smallint,
 	lote smallint,
 	vendido bool DEFAULT FALSE,
@@ -37,8 +37,8 @@ CREATE TABLE bilhete (
 );
 
 CREATE TABLE casamento (
-	data timestamp,
-	organizador char(11),
+	data timestamp NOT NULL,
+	organizador char(11) NOT NULL,
 	preco smallint NOT NULL,
 	conjuge1 varchar(50) NOT NULL,
 	conjuge2 varchar(50) NOT NULL,
@@ -48,15 +48,15 @@ CREATE TABLE casamento (
 );
 
 CREATE TABLE cerimonialista (
-	nome varchar(50),
+	nome varchar(50) NOT NULL,
 	telefone varchar(15),
 	linha_atuacao varchar(20),
 	CONSTRAINT pk_cerimonialista PRIMARY KEY (nome)
 );
 
 CREATE TABLE casamento_cerimonialista (
-	data timestamp,
-	organizador char(11),
+	data timestamp NOT NULL,
+	organizador char(11) NOT NULL,
 	cerimonialista varchar(50),
 	CONSTRAINT pk_casamento_cerimonialista PRIMARY KEY (data, organizador),
 	CONSTRAINT fk_casamento_cerimonialista_casa FOREIGN KEY (data, organizador)
@@ -66,13 +66,13 @@ CREATE TABLE casamento_cerimonialista (
 );
 
 CREATE TABLE empresa_tipo (
-	CNPJ char(14),
+	CNPJ char(14) NOT NULL,
 	tipo varchar(20) NOT NULL,
 	CONSTRAINT pk_empresa_tipo PRIMARY KEY (CNPJ)
 );
 
 CREATE TABLE locacao (
-	empresa char(14),
+	empresa char(14) NOT NULL,
 	telefone varchar(15),
 	email varchar(50),
 	endereco varchar(150),
@@ -81,7 +81,7 @@ CREATE TABLE locacao (
 );
 
 CREATE TABLE decoracao (
-	empresa char(14),
+	empresa char(14) NOT NULL,
 	telefone varchar(15),
 	email varchar(50),
 	endereco varchar(150),
@@ -90,7 +90,7 @@ CREATE TABLE decoracao (
 );
 
 CREATE TABLE floricultura (
-	empresa char(14),
+	empresa char(14) NOT NULL,
 	telefone varchar(15),
 	email varchar(50),
 	endereco varchar(150),
@@ -100,7 +100,7 @@ CREATE TABLE floricultura (
 );
 
 CREATE TABLE transporte (
-	empresa char(14),
+	empresa char(14) NOT NULL,
 	telefone varchar(15),
 	email varchar(50),
 	endereco varchar(150),
@@ -109,18 +109,18 @@ CREATE TABLE transporte (
 );
 
 CREATE TABLE decor (
-	tipo varchar(20),
-	cor varchar(20),
-	marca varchar(20),
+	tipo varchar(20) NOT NULL,
+	cor varchar(20) NOT NULL,
+	marca varchar(20) NOT NULL,
 	empresa char(14),
 	CONSTRAINT pk_decor PRIMARY KEY (tipo, cor, marca),
 	CONSTRAINT fk_decor FOREIGN KEY (empresa) REFERENCES decoracao(empresa)
 );
 
 CREATE TABLE casamento_decoracao (
-	data timestamp,
-	organizador char(11),
-	empresa char(14),
+	data timestamp NOT NULL,
+	organizador char(11) NOT NULL,
+	empresa char(14) NOT NULL,
 	preco smallint NOT NULL,
 	CONSTRAINT pk_casamento_decoracao PRIMARY KEY (data, organizador, empresa),
 	CONSTRAINT fk_casamento_decoracao_casa FOREIGN KEY (data, organizador)
@@ -130,17 +130,17 @@ CREATE TABLE casamento_decoracao (
 );
 
 CREATE TABLE flor(
-	empresa char(14),
-	especie varchar(20),
-	cor varchar(20),
+	empresa char(14) NOT NULL,
+	especie varchar(20) NOT NULL,
+	cor varchar(20) NOT NULL,
 	CONSTRAINT pk_flor PRIMARY KEY (empresa, especie, cor),
 	CONSTRAINT fk_flor FOREIGN KEY (empresa) REFERENCES floricultura(empresa)
 );
 
 CREATE TABLE casamento_floricultura (
-	data timestamp,
-	organizador char(11),
-	empresa char(14),
+	data timestamp NOT NULL,
+	organizador char(11) NOT NULL,
+	empresa char(14) NOT NULL,
 	preco smallint NOT NULL,
 	CONSTRAINT pk_casamento_floricultura PRIMARY KEY
 		(data, organizador, empresa),
@@ -152,23 +152,23 @@ CREATE TABLE casamento_floricultura (
 );
 
 CREATE TABLE veiculo_tipo (
-	modelo varchar(20),
+	modelo varchar(20) NOT NULL,
 	n_assentos smallint NOT NULL,
 	CONSTRAINT pk_veiculo_tipo PRIMARY KEY (modelo)
 );
 
 CREATE TABLE transporte_veiculo (
-	empresa char(14),
-	tipo varchar(20),
+	empresa char(14) NOT NULL,
+	tipo varchar(20) NOT NULL,
 	CONSTRAINT pk_transporte_veiculo PRIMARY KEY (empresa, tipo),
 	CONSTRAINT fk_transporte_veiculo_trans FOREIGN KEY (empresa)
 		REFERENCES transporte(empresa)
 );
 
 CREATE TABLE aluga (
-	data timestamp,
-	organizador char(11),
-	empresa char(14),
+	data timestamp NOT NULL,
+	organizador char(11) NOT NULL,
+	empresa char(14) NOT NULL,
 	CONSTRAINT pk_aluga PRIMARY KEY (data, organizador, empresa),
 	CONSTRAINT fk_aluga_univ FOREIGN KEY (data, organizador)
 		REFERENCES universitaria(data, organizador),
@@ -177,7 +177,7 @@ CREATE TABLE aluga (
 );
 
 CREATE TABLE espaco (
-	CEP char(8),
+	CEP char(8) NOT NULL,
 	categoria varchar(30),
 	endereco varchar(200),
 	capacidade integer,
@@ -187,8 +187,8 @@ CREATE TABLE espaco (
 );
 
 CREATE TABLE casamento_espaco (
-	data timestamp,
-	organizador char(11),
+	data timestamp NOT NULL,
+	organizador char(11) NOT NULL,
 	espaco char(8),
 	preco smallint NOT NULL,
 	CONSTRAINT pk_casamento_espaco PRIMARY KEY (data, organizador),
@@ -199,8 +199,8 @@ CREATE TABLE casamento_espaco (
 );
 
 CREATE TABLE universitaria_espaco (
-	data timestamp,
-	organizador char(11),
+	data timestamp NOT NULL,
+	organizador char(11) NOT NULL,
 	espaco char(8),
 	preco smallint NOT NULL,
 	CONSTRAINT pk_universitaria_espaco PRIMARY KEY (data, organizador),
